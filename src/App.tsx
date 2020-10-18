31dc57adddc8d83ad20b18b38005c8f1d6b449e7
omirbeck <ommirtull@gmail.com>
2020-10-19 03:24:24 +0600

    feat: add material ui, change Sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -2,32 +2,53 @@ import React, { useState, useRef, useEffect } from "react";
 import { ACCESS_KEY, API_URL } from "./credentials";
 import { Image } from './service/type';
 import "./App.css";
-import {useFetchData, useIntersactionObserver} from './service/hooks';
+import { useFetchData, useIntersactionObserver } from './service/hooks';
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import ImageList from "./components/ImageList";
-import { CircularProgress, Grid } from "@material-ui/core";
+import { CircularProgress, Grid, makeStyles, Typography } from "@material-ui/core";
+
 
 const INITIAL_PAGE = 1;
+
+const useStyles = makeStyles((theme) => ({
+  loading: {
+    padding: theme.spacing(2),
+  },
+  title: {
+    padding: theme.spacing(2, 0),
+  }
+}))
 
 function App() {
   const [images, setImages] = useState<Image[]>([]);
   const [page, setPages] = useState(INITIAL_PAGE);
+  const [open, setOpen] = useState(false);
   const [path, setPath] = useState<string>('');
   const [query, setQuery] = useState<string>();
-  const {data, loading, hasError} = useFetchData(path);
-  const {loader, hasInterSection} = useIntersactionObserver(); 
-  
+  const { data, loading, hasError } = useFetchData(path);
+  const { loader, hasInterSection } = useIntersactionObserver();
+
+  const classes = useStyles();
+
   const params = `page=${page}&per_page=20&client_id=${ACCESS_KEY}`;
 
-  const handleClick = (newQuery? : string) => {
+  const handleClick = (newQuery?: string) => {
     if (newQuery !== query) {
       setImages([]);
       setPages(INITIAL_PAGE);
     }
     setQuery(newQuery);
   };
-  
+
+  const handleSidebarOpen = () => {
+    setOpen(true);
+  }
+
+  const handleSidebarClose = () => {
+    setOpen(false);
+  }
+
   useEffect(() => {
     setImages(images => [...images, ...data])
   }, [data]);
@@ -46,10 +67,27 @@ function App() {
 
   return (
     <Grid container direction="column" justify="center">
-      <Header />
-      <Sidebar handleClick={handleClick} />
-      <ImageList images={images} />
-  <div ref={loader}>{loading && <CircularProgress /> }</div>
+      <Header 
+        open={open} 
+        handleSidebarOpen={handleSidebarOpen} />
+      <Sidebar 
+        open={open}
+        handleSidebarClose={handleSidebarClose}
+        handleClick={handleClick} />
+      <Typography
+        variant="h2"
+        align="center"
+        color="primary"
+        className={classes.title}>{query || 'random'}</Typography>
+      {images.length > 0 && <ImageList images={images} />}
+      {hasError &&
+        <Typography
+          variant="h4"
+          color="secondary"
+          align="center">Someting gues wrong...</Typography>}
+      <div
+        ref={loader}
+        className={classes.loading}>{loading && <CircularProgress />}</div>
     </Grid>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,44 @@
 import React from 'react';
+import { AppBar, makeStyles, Toolbar, Typography } from '@material-ui/core';
+import PhotoCameraIcon from '@material-ui/icons/PhotoCamera';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import { SIDEBAR_WIDTH } from './Sidebar';
 
-const Header = () => {
+
+const useStyle = makeStyles((theme) => ({
+    icon: {
+        marginRight: theme.spacing(2)
+    },
+    appBar: {
+        zIndex: theme.zIndex.drawer + 1
+    },
+    menuIcon: {
+        color: 'white'
+    },
+    appBarShift: {
+        width: `calc(100% - ${SIDEBAR_WIDTH})`,
+        marginLeft: SIDEBAR_WIDTH
+    }
+}))
+
+const Header = ({open, handleSidebarOpen}: {open: boolean, handleSidebarOpen: () => void}) => {
+    const classes = useStyle();
+
     return (
-        <header className="header">
-            <h1>Fancy Gallery</h1>
-        </header>
+        <AppBar position="sticky" 
+            className={open ? classes.appBarShift : classes.appBar}>
+            <Toolbar>
+                <IconButton onClick={handleSidebarOpen}>
+                    <MenuIcon 
+                        className={classes.menuIcon} 
+                        style={{ display: open ? 'none' : 'flex'}} />
+                </IconButton>
+                <PhotoCameraIcon className={classes.icon} />
+                <Typography 
+                    variant="h6">Fancy Gallery</Typography>
+            </Toolbar>
+        </AppBar>
     )
 }
 
